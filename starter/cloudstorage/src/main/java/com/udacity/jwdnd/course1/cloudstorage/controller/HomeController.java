b154e163fbde3533ed6add28f276a19bdbaf9941
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2020-09-26 08:28:59 +0700

    add mapping / for home page

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping({"/","/home"})
 public class HomeController {
     private NoteService noteService;
 
