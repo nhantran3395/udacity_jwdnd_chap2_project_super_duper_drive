7b33bf3aad52238863f5c29c286a286fa45302f1
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2020-09-27 15:40:30 +0700

    refactor to use separate templates and mappings for each tab

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -4,10 +4,9 @@ import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @Controller
 @RequestMapping({"/","/home"})
@@ -19,16 +18,39 @@ public class HomeController {
     }
 
     @GetMapping
-    public String getHomePage(@ModelAttribute("newNote") Note note, Model model){
-        model.addAttribute("noteList",noteService.getNotes());
+    public String getHomePage(Model model){
+        model.addAttribute("isFilesActive",true);
+        model.addAttribute("isNotesActive",false);
+        model.addAttribute("isCredentialsActive",false);
 
         return "home";
     }
 
-    @PostMapping
+    @GetMapping("files")
+    public String getFilesTab(){
+            return "_" + "files";
+    }
+
+    @GetMapping("notes")
+    public String getNotesTab(@ModelAttribute("newNote") Note note, Model model){
+        model.addAttribute("noteList",noteService.getNotes());
+
+        return "_" + "notes";
+    }
+
+    @GetMapping("credentials")
+    public String getCredentialsTab(){
+            return "_" + "credentials";
+    }
+
+    @PostMapping("notes")
     public String createItem(@ModelAttribute("newNote") Note note, Model model){
         noteService.createNote(note);
         model.addAttribute("noteList",noteService.getNotes());
+
+        model.addAttribute("isFilesActive",false);
+        model.addAttribute("isNotesActive",true);
+        model.addAttribute("isCredentialsActive",false);
 
         return "home";
     }
