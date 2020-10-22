ae4244dd36435cc37d19623678d4f92fb8f849cc
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2020-10-22 13:16:28 +0700

    config file upload maximum size. delete old upload dir on init

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -19,7 +19,7 @@ public class CloudStorageApplication {
 	@Bean
 	CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
-//			storageService.deleteAll();
+			storageService.deleteAll();
 			storageService.init();
 		};
 	}
