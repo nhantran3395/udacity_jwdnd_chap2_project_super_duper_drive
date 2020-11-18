79fbc5db542cac7800fffdf7e8791992b8561b9e
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2020-11-18 21:37:45 +0700

    add tests for files page

---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/UI/FileTests.java
@@ -0,0 +1,147 @@
+package com.udacity.jwdnd.course1.cloudstorage.UI;
+
+import com.udacity.jwdnd.course1.cloudstorage.UI.Page.FilePage;
+import com.udacity.jwdnd.course1.cloudstorage.UI.Page.LoginPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FileTests {
+
+    @LocalServerPort
+    public int port;
+
+    public static WebDriver driver;
+
+    public String baseURL;
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+        driver = null;
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        baseURL = "http://localhost:" + port;
+    }
+
+    @Test
+    @Order(1)
+    public void testUploadFileSuccess() {
+        String username = "marcianichols4792";
+        String password = "mason1";
+
+        String filePath = "D:\\Back_end\\Java\\udacity_jwdnd_super_duper_drive\\file_upload_test_data\\bear.jpg";
+        String fileName = "bear.jpg";
+
+        String alertSuccessFileUploadText = "Great! The file has been stored.\n×";
+
+        driver.get(baseURL + "/login");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(username, password);
+
+        driver.get(baseURL + "/files");
+
+        FilePage filePage = new FilePage(driver);
+
+        filePage.uploadFile(filePath);
+
+        assertEquals(alertSuccessFileUploadText,filePage.getAlertSuccessFileUploadText());
+        assertEquals(fileName,filePage.getUploadFilename());
+    }
+
+    @Test
+    @Order(3)
+    public void testDeleteFileSuccess() {
+        String username = "marcianichols4792";
+        String password = "mason1";
+
+        String alertSuccessFileRemoveText = "The file has been removed\n×";
+
+        driver.get(baseURL + "/login");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(username, password);
+
+        driver.get(baseURL + "/files");
+
+        FilePage filePage = new FilePage(driver);
+
+        filePage.deleteFile();
+
+        assertEquals(alertSuccessFileRemoveText,filePage.getAlertSuccessFileRemoveText());
+    }
+
+    @Test
+    public void testUploadEmptyFile() {
+        String username = "marcianichols4792";
+        String password = "mason1";
+
+        String filePathEmptyFile = "D:\\Back_end\\Java\\udacity_jwdnd_super_duper_drive\\file_upload_test_data\\empty.txt";
+        String fileNameEmptyFile = "empty.txt";
+
+        String alertErrorFileStorageFailure = "Oops! Something went wrong and we could not store the file. Please try again.\n"
+                +"Cause: Failed to store empty file empty.txt\n×";
+
+        driver.get(baseURL + "/login");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(username, password);
+
+        driver.get(baseURL + "/files");
+
+        FilePage filePage = new FilePage(driver);
+
+        filePage.uploadFile(filePathEmptyFile);
+
+        assertTrue(filePage.checkIfAlertErrorFileStorageFailureIsDisplayed());
+        assertEquals(alertErrorFileStorageFailure,filePage.getAlertErrorFileStorageFailureText());
+
+        assertThrows(NoSuchElementException.class,()->filePage.getUploadFilenameEmptyFile());
+    }
+
+    @Test
+    public void testUploadFileExceedLimit() {
+        String username = "marcianichols4792";
+        String password = "mason1";
+
+        String filePathExceedLimitFile = "D:\\Back_end\\Java\\udacity_jwdnd_super_duper_drive\\file_upload_test_data\\heavy_carrot.jpg";
+        String fileNameExceedLimitFile = "heavy_carrot.jpg";
+
+        String alertErrorFileSizeLimitExceeded = "Oops! Something went wrong and we could not get the file from storage. Please try again.\n"
+                +"Cause: Maximum upload size exceeded\n×";
+
+        driver.get(baseURL + "/login");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(username, password);
+
+        driver.get(baseURL + "/files");
+
+        FilePage filePage = new FilePage(driver);
+
+        filePage.uploadFile(filePathExceedLimitFile);
+
+        assertTrue(filePage.checkIfAlertErrorFileSizeLimitExceeded());
+        assertEquals(alertErrorFileSizeLimitExceeded,filePage.getAlertErrorFileSizeLimitExceededText());
+
+        assertThrows(NoSuchElementException.class,()->filePage.getUploadFilenameSizeLimitExceeded());
+    }
+
+}
---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/UI/Page/FilePage.java
@@ -0,0 +1,98 @@
+package com.udacity.jwdnd.course1.cloudstorage.UI.Page;
+
+import com.udacity.jwdnd.course1.cloudstorage.UI.Page.PageUtil.PageUtil;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FilePage {
+
+    private PageUtil pageUtil;
+
+    public FilePage(WebDriver driver) {
+        this.pageUtil = new PageUtil(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id="fileUpload")
+    private WebElement fileInput;
+
+    @FindBy(id="file-upload-button")
+    private WebElement fileUploadButton;
+
+    @FindBy(id="alert-success-file-upload")
+    private WebElement alertSuccessFileUpload;
+
+    @FindBy(id="alert-success-file-remove")
+    private WebElement alertSuccessFileRemove;
+
+    @FindBy(id="alert-error-file-storageFailure")
+    private WebElement alertErrorFileStorageFailure;
+
+    @FindBy(id="alert-error-file-sizeLimitExceeded")
+    private WebElement alertErrorFileSizeLimitExceeded;
+
+    @FindBy(id="uploaded-filename-bear.jpg")
+    private WebElement uploadedFilename;
+
+    @FindBy(id="uploaded-filename-empty.txt")
+    private WebElement uploadedFilenameEmptyFile;
+
+    @FindBy(id="uploaded-filename-heavy-carrot.txt")
+    private WebElement uploadedFilenameSizeLimitExceeded;
+
+    @FindBy(id="uploaded-file-delete-button-bear.jpg")
+    private WebElement uploadedFileDeleteButton;
+
+    @FindBy(id="uploaded-file-view-button-bear.jpg")
+    private WebElement uploadedFileViewButton;
+
+
+    public void uploadFile(String filepath){
+        fileInput.sendKeys(filepath);
+        fileUploadButton.click();
+    }
+
+    public void deleteFile(){
+        uploadedFileDeleteButton.click();
+    }
+
+    public String getAlertSuccessFileUploadText(){
+        return alertSuccessFileUpload.getText();
+    }
+
+    public String getAlertSuccessFileRemoveText(){
+        return alertSuccessFileRemove.getText();
+    }
+
+    public boolean checkIfAlertErrorFileStorageFailureIsDisplayed(){
+        return alertErrorFileStorageFailure.isDisplayed();
+    }
+
+    public String getAlertErrorFileStorageFailureText(){
+        return alertErrorFileStorageFailure.getText();
+    }
+
+    public boolean checkIfAlertErrorFileSizeLimitExceeded(){
+        return alertErrorFileSizeLimitExceeded.isDisplayed();
+    }
+
+    public String getAlertErrorFileSizeLimitExceededText(){
+        return alertErrorFileSizeLimitExceeded.getText();
+    }
+
+
+    public String getUploadFilename(){
+        return uploadedFilename.getText();
+    }
+
+    public String getUploadFilenameEmptyFile(){
+        return uploadedFilenameEmptyFile.getText();
+    }
+
+    public String getUploadFilenameSizeLimitExceeded(){
+        return uploadedFilenameSizeLimitExceeded.getText();
+    }
+
+}
